a210f7cf75dc02c494069bc69f3a12a7329e3af8
Mamdouh El Nakeeb <mamdouh.el.nakeeb@gmail.com>
2017-11-03 02:23:21 +0200

    Pods dir removal

--- iOS/Hegg/Pods/Target Support Files/NOCProtoKit/NOCProtoKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_NOCProtoKit : NSObject
-@end
-@implementation PodsDummy_NOCProtoKit
-@end
--- iOS/Hegg/Pods/Target Support Files/Pods-Hegg/Pods-Hegg-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Hegg : NSObject
-@end
-@implementation PodsDummy_Pods_Hegg
-@end
